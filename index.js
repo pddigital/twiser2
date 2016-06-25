a8e262d5545d9a113c816a4c87e441201117a8b6
Paul Day <paul.day.m@gmail.com>
2016-06-25 16:46:09 -0500

    setting up

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const {json} = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const corsOptions = {
-  origin: 'http://localhost:9000'
+  origin: 'http://localhost:5000'
 }
-const port = 9000;
+const port = 5000;
 const masterRoutes = require('./server/twiserRoutes')
 
 const app = express();
-const mongoUri = `mongodb://heroku_5l344rtz:9umf8hib7basg24ts73223okkc@ds023704.mlab.com:23704/heroku_5l344rtz`;
 
-mongoose.connect(mongoUri);
-mongoose.connection.once(`open`, ()=> {
-  console.log(`Connected to mongo at ${mongoUri}`)
-})
+mongoose.connect(MONGODB_URI);
+// mongoose.connection.once(`open`, ()=> {
+//   console.log(`Connected to mongo at ${mongoUri}`)
+// })
 
 app.use(json());
 
