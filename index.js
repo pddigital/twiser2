9d4354c9a8d4e463191f338ea8cb9bc5e2135c43
Paul Day <paul.day.m@gmail.com>
2016-06-26 10:21:46 -0500

    Another change

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const {json} = require('body-parser')
 const mongoose = require('mongoose')
-const cors = require('cors')
+// const cors = require('cors')
 const port = 9000;
 const masterRoutes = require('./server/twiserRoutes')
-const corsOptions = {
-  origin: 'http://localhost:9000'
-}
+// const corsOptions = {
+//   origin: 'http://localhost:9000'
+// }
 
 const app = express();
 
